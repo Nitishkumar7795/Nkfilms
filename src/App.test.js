c3bedd11be70728b5b47b36a159c306ed7fc88eb
Nitish Kumar <nitishnatikar360@gmail.com>
2026-04-29 16:18:25 +0530

    removed test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders NKFILMS app', () => {
-  render(<App />);
-  const logoElement = screen.getByText(/NKFILMS/i);
-  expect(logoElement).toBeInTheDocument();
-});
