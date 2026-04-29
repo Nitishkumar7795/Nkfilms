192515ad6d62aeadf9b619141ce37a59618cafd0
Nitish Kumar <nitishnatikar360@gmail.com>
2026-04-29 16:14:19 +0530

    test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders NKFILMS app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoElement = screen.getByText(/NKFILMS/i);
+  expect(logoElement).toBeInTheDocument();
 });
